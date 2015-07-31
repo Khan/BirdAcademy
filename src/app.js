a65b3d519193938ab8c95d8349c25d1a2868556d
Andy Matuschak <andy@khanacademy.org>
2015-07-30 17:28:31 -0700

    Bird that floats and can land

--- src/app.js
+++ src/app.js
@@ -1,7 +1,5 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
-ctx.fillStyle = "rgb(0, 0, 255)";
-ctx.fillRect(10, 10, 100, 100);
 
 function loadImage(name) {
 	const image = new Image();
@@ -10,12 +8,73 @@ function loadImage(name) {
 }
 
 const mamaCloud = loadImage("mama cloud");
-mamaCloud.currentX = 100
+mamaCloud.currentX = 100;
 
 const weeCloud = loadImage("wee cloud");
-weeCloud.currentX = 400
+weeCloud.currentX = 400;
 
-const fencePosts = loadImage("fence")
+const fencePosts = loadImage("fence");
+const firstFencePostX = 643;
+const fencePostSpacing = 78;
+const fencePostY = 695;
+
+class Bird {
+	constructor(x, y) {
+		this.x = x;
+		this.y = y;
+		this.targetX = this.x;
+		this.targetY = this.y;
+		this.width = 50;
+		this.height = 50;
+		this.flying = true;
+	}
+
+	update() {
+		if (!this.dragged) {
+			if (Mouse.pressed && !this.lastPressed) {
+				const dx = Mouse.x - this.x;
+				const dy = Mouse.y - this.y;
+				if (Math.abs(dx) < this.width / 2.0 && Math.abs(dy) < this.height / 2.0) {
+					this.dragged = true;
+				}
+			}
+		} else {
+			if (!Mouse.pressed) {
+				if (this.flying) {
+					this.targetX = firstFencePostX;
+					this.targetY = fencePostY - this.height / 2.0;
+					this.flying = false;
+				} else {
+					this.targetX = 100;
+					this.targetY = 100;
+					this.flying = true;
+				}
+				this.dragged = false;
+			}
+		}
+
+		this.lastPressed = Mouse.pressed
+
+		const speed = 0.15;
+		this.x = this.x * (1.0 - speed) + this.targetX * speed;
+		this.y = this.y * (1.0 - speed) + this.targetY * speed;
+
+		if (this.flying) {
+			this.x += Math.sin(Date.now() / 1000) * 5
+			this.y += Math.sin(Date.now() / 1250) * 3
+		}
+	}
+
+	draw() {
+		ctx.save();
+		ctx.beginPath();
+		ctx.ellipse(this.x, this.y, this.width / 2.0, this.height / 2.0, 0, 0, 2 * Math.PI)
+		ctx.fill();
+		ctx.restore();
+	}
+}
+
+const bird = new Bird(100, 100);
 
 window.render = () => {
 	mamaCloud.currentX -= 0.3
@@ -34,6 +93,9 @@ window.render = () => {
 	ctx.drawImage(weeCloud, weeCloud.currentX, 150);
 
 	ctx.drawImage(fencePosts, 0, 0);
+
+	bird.update();
+	bird.draw();
 }
 
 
